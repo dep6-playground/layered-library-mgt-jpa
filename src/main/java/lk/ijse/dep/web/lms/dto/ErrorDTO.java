0e37078426b793a25d76c88854d87d432ba68d4e
Uvin Weththasinghe <uvin6667@gmail.com>
2021-02-05 14:54:48 +0530

    Establish util, listener, exception packages and error dto file

---
+++ src/main/java/lk/ijse/dep/web/lms/dto/ErrorDTO.java
@@ -0,0 +1,15 @@
+package lk.ijse.dep.web.lms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data @AllArgsConstructor @NoArgsConstructor
+public class ErrorDTO implements Serializable {
+    private int status;
+    private String error;
+    private String message;
+
+}
